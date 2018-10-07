737d3c1974d430e8392dfa6eb9e83a930cb87e55
Vytautas Galaunia <vytautas.galaunia@gmail.com>
2018-10-07 13:32:07 +0300

    Fix in memory store and and filtering

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,10 @@ const routes: Routes = [
         component: ResultComponent
     },
     {
+        path: 'tours/:keyword',
+        component: ToursComponent
+    },
+    {
         path: 'tours',
         component: ToursComponent
     },
--- src/app/in-memory-tours.service.ts
+++
@@ -1,9 +0,0 @@
-  import { InMemoryDbService } from 'angular-in-memory-web-api';
-
-  import * as allTours from './tours.json';
-
-  export class InMemoryToursService implements InMemoryDbService {
-    createDb() {
-      return { 'tours': allTours }
-    }
-  }
