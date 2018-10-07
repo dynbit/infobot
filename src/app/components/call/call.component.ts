2563c48b2f614de27cf6e8aa4eaacfb81d2fea8a
Pranas Kiziela <pranas.kiziela@gmail.com>
2018-10-07 11:15:32 +0300

    Bring back NLP call

--- src/app/components/call/call.component.ts
+++ src/app/components/call/call.component.ts
@@ -84,7 +84,7 @@ export class CallComponent implements OnInit {
       };
 
       this.recognition.onend = function() {
-        
+
         recognizing = false;
 
         if (ignore_onend) {
@@ -103,22 +103,21 @@ export class CallComponent implements OnInit {
 
         for (var i = event.resultIndex; i < event.results.length; ++i) {
           if (event.results[i].isFinal) {
-
-            final_transcript += event.results[i][0].transcript;
-            
-            _self.recognition.stop();
-
-            console.log(final_transcript)
+            var transcript = event.results[i][0].transcript
+            console.log("Final transcript: ", transcript)
+            processText(transcript, function(a,b) {
+              console.log("Intent: ", b.intent.name)
+              console.log("Entities: ", b.entities)
+            })
 
             interim_transcript = '';
             final_transcript = '';
-
           } else {
             interim_transcript += event.results[i][0].transcript;
-
-            console.log(interim_transcript)
           }
         }
+
+        console.log(interim_transcript)
 
       };
 
