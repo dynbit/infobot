4c0cb2d562dcc949206775a55b3e2d9832a5c513
Petras Baukys <petrasb@gmail.com>
2018-10-07 15:51:48 +0300

    Minor fix

--- src/app/components/call/call.component.ts
+++ src/app/components/call/call.component.ts
@@ -132,7 +132,7 @@ export class CallComponent implements OnInit {
                   }]);
                 });
               } else {
-                this.startRecognition()
+                _self.startRecognition()
               }
 
             })
